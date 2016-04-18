c4362df361e52969d0847830d2d7219e88eb9ffa
Usman Irale <usman@hotels.ng>
2016-04-18 23:55:55 +0100

    first

---
+++ app/models/user.js
@@ -0,0 +1,37 @@
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('twitapp', 'root', 'omokhudu', {
+  host: 'localhost',
+  dialect: 'mysql',
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
+});
+
+var User = sequelize.define('users', {
+    name: {
+    type: Sequelize.STRING,
+    field: 'name' // Will result in an attribute that is firstName when user facing but first_name in the database
+  },
+  userName: {
+    type: Sequelize.STRING,
+    field: 'username'
+  },
+  passWord:{
+    type: Sequelize.STRING,
+    field: 'password'
+  }
+}, {
+  freezeTableName: true // Model tableName will be the same as the model name
+});
+
+// User.sync({force: true}).then(function () {
+//   // Table created
+//   return User.create({
+//     name: 'Usman Irale',
+//     userName: 'superirale'
+//   });
+// });
+
+module.export = User;
